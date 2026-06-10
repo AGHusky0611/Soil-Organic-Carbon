bc13eb664222529f3b7076cc9a1ef62fe159eccc
kurudev <kurisitiyan.0611@gmail.com>
2026-06-10 14:22:00 +0800

    Refactor preprocess_image method in LabColorExtractor for improved image quality

--- ClassificationModels/CLS_extraction.py
+++ ClassificationModels/CLS_extraction.py
@@ -30,6 +30,23 @@ class LabColorExtractor:
         standardized = cv2.normalize(standardized, None, 0, 255, cv2.NORM_MINMAX)
         return standardized
 
+    def preprocess_image(self, img):
+        """
+        Preprocess image for better visual quality
+        
+        Steps:
+        1. White balance correction
+        2. Light denoising (Gaussian blur)
+        3. Dimensional standardization
+        """
+        if img is None:
+            return None
+        
+        wb_img = self._white_balance(img)
+        denoised = self._denoise(wb_img)
+        standardized = self._dimensional_standardization(denoised)
+        return standardized
+
     def _extract_glcm_features(self, gray):
         """Extract GLCM (Gray Level Co-occurrence Matrix) texture features"""
         # Reduce bit depth for GLCM computation
@@ -43,7 +60,7 @@ class LabColorExtractor:
         
         for distance in distances:
             for angle in angles:
-                raycomatrix(
+                glcm = graycomatrix(
                     gray_reduced, 
                     distances=[distance], 
                     angles=[angle], 
@@ -108,20 +125,3 @@ class LabColorExtractor:
         lab_features = np.array([mean_l, mean_a, mean_b, std_l, std_a, std_b, laplacian_var])
         
         return np.hstack([glcm_features, lab_features])
-
-    def preprocess_image(self, img):
-        """
-        Preprocess image for better visual quality
-        
-        Steps:
-        1. White balance correction
-        2. Light denoising (Gaussian blur)
-        3. Dimensional standardization
-        """
-        if img is None:
-            return None
-        
-        wb_img = self._white_balance(img)
-        denoised = self._denoise(wb_img)
-        standardized = self._dimensional_standardization(denoised)
-        return standardized
